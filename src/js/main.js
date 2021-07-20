3c2eb44462c6e7d2e901ae2da70a79b95ab47c72
fadiKaba <fadikaba9@gmail.com>
2021-07-20 11:48:15 +0200

    validation is good

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,12 @@ const cartContainer = document.querySelector('#cart-container tbody');
 
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const talent = document.querySelector('#talent');
+const address = document.querySelector('#address');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
 
 let products = [
     {id: 1,name: 'product 1', price: 400, src: '../src/imgs/product1.jpeg'},
@@ -165,7 +171,6 @@ function setCookie(cname, cvalue, exdays) {
  }
 
  function normalTextValidation(input, feedBack){
-     let text = '';
          if(input.value.length < 2) {
             input.classList.add('is-invalid');
             document.querySelector('#'+input.getAttribute('id')+'-feedback').innerHTML = feedBack;
@@ -175,10 +180,70 @@ function setCookie(cname, cvalue, exdays) {
      return true;  
  }
 
+ function phoneValidation(input, feedBack){
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    if(!re.test(String(input.value).toLowerCase())) {
+       input.classList.add('is-invalid');
+       document.querySelector('#'+input.getAttribute('id')+'-feedback').innerHTML = feedBack;
+       return false;
+    }   
+    input.classList.remove('is-invalid');
+    return true;  
+}
+
+function genderValidate(feedBack){
+  let gender = document.querySelectorAll('input[name=gender]:checked');
+  document.querySelector('#gender-feedback small').innerHTML= feedBack;
+  if(gender.length < 1){
+      document.querySelector('#gender-feedback').classList.remove('d-none');
+    return false;
+  }
+  document.querySelector('#gender-feedback').classList.add('d-none');
+  return true; 
+}
+
+ function emailValidation(input, feedBack){
+        
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(String(input.value).toLowerCase())) {
+        input.classList.add('is-invalid');
+        document.querySelector('#'+input.getAttribute('id')+'-feedback').innerHTML = feedBack;
+        return false;
+    }   
+    input.classList.remove('is-invalid');
+    return true;  
+}
+
+function passwordValidate(input1, input2){
+  
+    if(input1.value.length < 8){
+        input1.classList.add('is-invalid');
+        document.querySelector('#'+input1.getAttribute('id')+'-feedback').innerHTML = 'Password length should not be less than 8 characters';
+        return false;
+    }
+    if(input1.value != input2.value){
+        input1.classList.add('is-invalid');
+        input2.classList.add('is-invalid');
+        document.querySelector('#'+input1.getAttribute('id')+'-feedback').innerHTML = 'The password field and the confirm password field do not match';
+        return false;
+    }
+    input2.classList.remove('is-invalid');
+    input1.classList.remove('is-invalid');
+    return true
+
+}
+
  function formSubmit(f){
     let fields = [
         normalTextValidation(firstName, 'First name must contains more than on character'),
         normalTextValidation(lastName, 'Last name must contains more than on character'),
+        emailValidation(email, 'Email is not valid'),
+     //   phoneValidation(phone, 'Phone is not valid'),
+        normalTextValidation(phone, 'Phone is required'),
+        genderValidate('Gender is required'),
+        normalTextValidation(talent, 'Talent is required'),
+        normalTextValidation(address, 'Address is required'),
+        passwordValidate(password, confirmPassword)
     ];
     console.log(fields)
     fields.forEach(el => {
